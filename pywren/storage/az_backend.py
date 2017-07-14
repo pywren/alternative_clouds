55689d06fc942ffd58335251e2820ed191f28a99
Allan Peng <potette007@gmail.com>
2017-07-14 15:12:41 -0700

    az storage

--- pywren/storage/az_backend.py
+++ pywren/storage/az_backend.py
@@ -8,13 +8,12 @@ class AZBackend(object):
     """
 
     def __init__(self, config):
-
         self.container = config["container"]
         self.block_blob_service = BlockBlobService(
             account_name=config["account"],
-            account_key=config["key"]
+            account_key=config["key"],
         )
-        
+
     def put_object(self, key, data):
         """
         Put an object in az storage.
@@ -27,9 +26,8 @@ class AZBackend(object):
         self.block_blob_service.create_blob_from_bytes(
             container_name = self.container,
             blob_name = key,
-            blob = data
+            blob = data,
         )
-
 
     def get_object(self, key):
         """
@@ -40,7 +38,12 @@ class AZBackend(object):
         :rtype: str/bytes
         """
         try:
-            r = self.block_blob_service.get
+            r = self.block_blob_service.get_blob_to_bytes(
+                container = self.container,
+                blob_name = key,
+            )
+            return r.content
+
         except azure.common.AzureMissingResourceHttpError as e:
             raise StorageNoSuchKeyError(key)
 
@@ -56,12 +59,23 @@ class AZBackend(object):
     def list_keys_with_prefix(self, pref):
         """
         Return a list of keys for the given prefix.
-        :param key: key of the object
-        :return: True if key exists, False if not exists
-        :rtype: A list of keys
+        :param pref: filter prefix
+        :return: List of keys for the given prefix.
+        :rtype: List of str
         """
-        paginator = self.block_blob_service.list_blobs(
-            container = self.container,
-            prefix = pref
-        )
+        key_list = []
+        next_token = None
 
+        while True:
+            paginator = self.block_blob_service.list_blobs(
+                container = self.container,
+                prefix = pref,
+                marker = next_token,
+            )
+            for blob in paginator:
+                key_list.apend(blob.name)
+            next_token = paginator.next_marker
+            if len(next_token) == 0:
+                break
+
+        return key_list
